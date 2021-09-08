25415aaa5a42933720a7bfede084eeaabed01a68
AMITOJ SINGH <singhamitoj10@gmail.com>
2021-09-08 12:55:41 -0400

    initialized django files

---
+++ fitness_tracker_django/main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+
+class Home(View):
+
+    def get(self, request):
+        return HttpResponse('Fitness Home')
+
+
+# Create your views here.
