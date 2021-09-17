cb90ebb79eab4f21d12c957394e9c0652318e99b
AMITOJ SINGH <singhamitoj10@gmail.com>
2021-09-17 12:23:49 -0400

    animations added

--- fitness_tracker_django/main_app/urls.py
+++ fitness_tracker_django/main_app/urls.py
@@ -14,6 +14,4 @@ urlpatterns = [
   path('dashboard/<int:pk>/weight_delete/', views.WeightDelete.as_view(), name="weight_delete"),
   path('dashboard/createsleep/', views.SleepCreate.as_view(), name='sleep_create'),
   path('dashboard/<int:pk>/sleep_delete/', views.SleepDelete.as_view(), name="sleep_delete")
-  # path('regerror/', views.HomeError.as_view(), name='reg_error'),
-  # path('posts/<int:pk>/', views.PostDetails.as_view(),  name='post_details')
 ]
